1fda99ed5d9dda88e1836e37a27c2fe55af4086d
Christian Jensen <ccjensen1179@gmail.com>
2019-05-02 15:51:00 -0600

    Trying to make the header horizontal

--- web/Week02/Ponder02/header.php
+++ web/Week02/Ponder02/header.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="header.css">
 
-        <nav id="Header:">
-            <ul>
+        <nav id="Header">
+            <ul id="Horizontal_List">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="assignments.php">Assignments</a></li>
             </ul>
