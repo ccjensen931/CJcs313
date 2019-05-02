0be3bd7435a4500b436614ed9696691c345a12d7
Christian Jensen <ccjensen1179@gmail.com>
2019-05-02 15:31:44 -0600

    Working on header for Ponder02

--- web/Week02/Ponder02/index.php
+++ web/Week02/Ponder02/index.php
@@ -7,11 +7,18 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <?php
+      include 'header.php'
+    ?>
 
-    <title>Hello, world!</title>
+    <title>CS 313 Jensen</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div id="Intro">
+      <p>
+        This is a test.
+      </p>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
