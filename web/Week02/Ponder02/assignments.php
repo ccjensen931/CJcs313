6c5faccf345ebda4ef1959e8c7b84d71692de515
Christian Jensen <ccjensen1179@gmail.com>
2019-05-02 15:46:58 -0600

    Trying to beautify the header

--- web/Week02/Ponder02/assignments.php
+++ web/Week02/Ponder02/assignments.php
@@ -7,10 +7,15 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <?php
+      include 'header.php'
+    ?>
+    
     <title>Assignments</title>
   </head>
   <body>
-    
+    <ul>
+        <li><a href="../../Week01/Ponder01/hello.html">Ponder 01</a></li>
+    </ul>  
   </body>
 </html>
--- web/Week02/Ponder02/header.php
+++ web/Week02/Ponder02/header.php
@@ -7,10 +7,13 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="header.css">
 
-        <ul id="header">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="assignments.php">Assignments</a></li>
-        </ul>
+        <nav id="Header:">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="assignments.php">Assignments</a></li>
+            </ul>
+        </nav>
     </head>
 </html>
