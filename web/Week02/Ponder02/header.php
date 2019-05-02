3e16fbe67d197903a15671d538ab2e95c434bbed
Christian Jensen <ccjensen1179@gmail.com>
2019-05-02 15:59:52 -0600

    Bootstrap was being overridden.

--- web/Week02/Ponder02/header.php
+++ web/Week02/Ponder02/header.php
@@ -7,12 +7,11 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="header.css">
 
     <nav class="navbar navbar-default">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="#">WebSiteName</a>
+                <a class="navbar-brand" href="#">CS 313 Jensen</a>
             </div>
             <ul class="nav navbar-nav">
                 <li><a href="index.php">Home</a></li>
