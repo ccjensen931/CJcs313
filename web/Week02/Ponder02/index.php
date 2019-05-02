c9bad8c209c641f9cad25d994be67279aa8ed111
Christian Jensen <ccjensen1179@gmail.com>
2019-05-02 16:18:40 -0600

    Adding personal info

--- web/Week02/Ponder02/index.php
+++ web/Week02/Ponder02/index.php
@@ -14,6 +14,12 @@
     <title>Home</title>
   </head>
   <body>
-
+    <div class="card">
+      <img class="card-img-top" src="Images/Acoustic Guitar.jpg" alt="">
+      <div class="card-body">
+        <h4 class="card-title">Guitars</h4>
+        <p class="card-text">Something about me is I play guitar and have done so for seven years. I greatly enjoy it and play both acoustic and electric guitar.</p>
+      </div>
+    </div>
   </body>
 </html>
