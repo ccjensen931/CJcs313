a6ca657aa8a8448d702e481facb43be1e18c1723
Christian Jensen <ccjensen1179@gmail.com>
2019-05-02 15:35:38 -0600

    Working on header links

--- web/Week02/Ponder02/header.php
+++ web/Week02/Ponder02/header.php
@@ -8,11 +8,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <p>
-        Test header
-    </p>
-  </head>
-  <body>
-    
-  </body>
+        <ul id=header>
+            <link href="index.php">
+            <link href="assignments.php">
+        </ul>
+    </head>
 </html>
