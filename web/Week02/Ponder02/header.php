04f00d5839f53fa3805b8fa6d25ff48ebef2c620
Christian Jensen <ccjensen1179@gmail.com>
2019-05-02 15:38:23 -0600

    Trying to get links to work

--- web/Week02/Ponder02/header.php
+++ web/Week02/Ponder02/header.php
@@ -8,9 +8,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        <ul id=header>
-            <link href="index.php">
-            <link href="assignments.php">
+        <ul id="header">
+            <li><link href="index.php"></li>
+            <li><link href="assignments.php"></li>
         </ul>
     </head>
 </html>
