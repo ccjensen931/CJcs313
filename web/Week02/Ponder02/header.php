ae0e255deae5860e9d5211cb20724290a8795e78
Christian Jensen <ccjensen1179@gmail.com>
2019-05-02 15:40:17 -0600

    Forgot <a> tags

--- web/Week02/Ponder02/header.php
+++ web/Week02/Ponder02/header.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <ul id="header">
-            <li><link href="index.php"></li>
-            <li><link href="assignments.php"></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="assignments.php">Assignments</a></li>
         </ul>
     </head>
 </html>
