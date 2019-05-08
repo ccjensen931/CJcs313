8c1b3949f25b57e2d6e3e2b51db845c65c245db5
Christian Jensen <ccjensen1179@gmail.com>
2019-05-08 10:26:13 -0600

    I think I fixed the link

--- web/Week03/Ponder03/header.php
+++ web/Week03/Ponder03/header.php
@@ -13,7 +13,7 @@
 
     <title>Navbar</title>
 </head>
-<body>b4-nav
+<body>
     <nav class="navbar navbar-expand-sm navbar-light bg-light">
         <a class="navbar-brand" href="#">FunStuff.com</a>
         <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
@@ -22,7 +22,7 @@
         </button>
         <div class="collapse navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active mt-3">
+                <li class="nav-item active mt-2.5">
                     <a class="nav-link" href="browse.php">Browse Inventory<span class="sr-only">(current)</span></a>
                 </li>
             <form class="form-inline my-2 my-lg-0">
