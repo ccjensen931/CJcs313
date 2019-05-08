54a65fe9f2c3a4dbf15dcb9a3e1a5451628a5b96
Christian Jensen <ccjensen1179@gmail.com>
2019-05-08 10:37:28 -0600

    Override work, modifying values.

--- web/Week03/Ponder03/header.php
+++ web/Week03/Ponder03/header.php
@@ -22,7 +22,7 @@
         </button>
         <div class="collapse navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active" style="margin-top:50px">
+                <li class="nav-item active" style="margin-top:25px;">
                     <a class="nav-link" href="browse.php">Browse Inventory<span class="sr-only">(current)</span></a>
                 </li>
             <form class="form-inline ml-3 my-2 my-lg-0">
@@ -30,7 +30,7 @@
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
             <div class="navbar-nav ml-2 mt-2 mt-lg-0">
-                <a class="nav-link" href="cart.php" style="margin-right:20px">
+                <a class="nav-link" href="cart.php" style="margin-left:150px;margin-right:20px;">
                     <img src="Images/shopping_cart.png" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="" style="width:50px;height:50px;">
                     Items
                 </a>
