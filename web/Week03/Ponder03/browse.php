cd45074f67bec699af4e536ef0ab9bbe03ffcf3c
Christian Jensen <ccjensen1179@gmail.com>
2019-05-08 09:13:02 -0600

    Started Ponder03

---
+++ web/Week03/Ponder03/browse.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Fun Stuff Store</title>
+</head>
+<body>
+    
+<?php
+    $_SESSION["Test"] = "This is a test.";
+?>
+
+<nav class="nav justify-content-center">
+  <a class="nav-link" href="cart.php">Cart</a>
+</nav>
+
+</body>
+</html>
