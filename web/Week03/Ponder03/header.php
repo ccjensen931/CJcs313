6f27d91c6ed858988f9a4ede4e97b185efbe2049
Christian Jensen <ccjensen1179@gmail.com>
2019-05-08 10:27:01 -0600

    Definitely made it worse, this chouls be close enough.

--- web/Week03/Ponder03/header.php
+++ web/Week03/Ponder03/header.php
@@ -22,7 +22,7 @@
         </button>
         <div class="collapse navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active mt-2.5">
+                <li class="nav-item active mt-3">
                     <a class="nav-link" href="browse.php">Browse Inventory<span class="sr-only">(current)</span></a>
                 </li>
             <form class="form-inline my-2 my-lg-0">
