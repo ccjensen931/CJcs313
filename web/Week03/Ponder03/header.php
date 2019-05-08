da254b22a575360ad547c06548d59f452b10f6e5
Christian Jensen <ccjensen1179@gmail.com>
2019-05-08 11:09:39 -0600

    Removing stylesheet because it broke stuff.

--- web/Week03/Ponder03/header.php
+++ web/Week03/Ponder03/header.php
@@ -13,8 +13,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="header.css">
-
+    
     <title>Navbar</title>
 </head>
 <body>
@@ -26,7 +25,7 @@
         </button>
         <div class="collapse navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
+                <li class="nav-item active" style="margin-top:15px;">
                     <a class="nav-link" href="browse.php">Browse Inventory<span class="sr-only">(current)</span></a>
                 </li>
             <form class="form-inline ml-3 my-2 my-lg-0">
@@ -34,7 +33,7 @@
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
             <div class="navbar-nav ml-2 mt-2 mt-lg-0">
-                <a class="nav-link" href="cart.php">
+                <a class="nav-link" href="cart.php" style="margin-left:600px;margin-right:20px;">
                     <img src="Images/shopping_cart.png" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="" style="width:50px;height:50px;">
                     <?php
                         echo $count;
