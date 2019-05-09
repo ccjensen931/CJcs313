dfe54a3ded0e052d49f3c14d5a98269ab7bc7ac3
ccjensen931 <ccjensen1179@gmail.com>
2019-05-08 19:02:23 -0600

    Forgot to make search a form

--- web/Week03/Ponder03/header.php
+++ web/Week03/Ponder03/header.php
@@ -29,7 +29,7 @@
                     <a class="nav-link" href="browse.php">Browse Inventory<span class="sr-only">(current)</span></a>
                 </li>
             </ul>
-            <form class="form-inline ml-3 my-2 my-lg-0">
+            <form action="browse.php" method="post" class="form-inline ml-3 my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="text" placeholder="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
