9fc0f7fbd4247c0c5a23dee3e64ffa63da500bba
ccjensen931 <ccjensen1179@gmail.com>
2019-05-10 07:19:49 -0600

    Increasing Column size.

--- web/Week02/Ponder02/assignments.php
+++ web/Week02/Ponder02/assignments.php
@@ -17,16 +17,16 @@
     <img src="Images/php.png" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="">
     <div class="container">
         <div class = "row">
-            <div class="col-1">
+            <div class="col-2">
                 <a href="../../Week01/Ponder01/hello.html"><button type="button" class="btn btn-primary">Ponder 01</button></a>
             </div>
-            <div class="col-1">
+            <div class="col-2">
                 <a href="../../Week02/Ponder02/index.php"><button type="button" class="btn btn-primary">Ponder 02</button></a>
             </div>
-            <div class="col-1">
+            <div class="col-2">
                 <a href="../../Week03/TeamActivity03/home.php"><button type="button" class="btn btn-primary">Team Activity 03</button></a>
             </div>
-            <div class="col-1">
+            <div class="col-2">
                 <a href="../../Week03/Ponder03/browse.php"><button type="button" class="btn btn-primary">Ponder 03</button></a>
             </div>
         </div>
