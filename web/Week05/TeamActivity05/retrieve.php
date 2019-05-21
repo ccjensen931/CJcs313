3d85d14072aa73d463e4adef0fe995b1c38e86da
Christian Jensen <ccjensen1179@gmail.com>
2019-05-21 16:20:22 -0600

    Restructing file system.

--- web/Week05/TeamActivity05/retrieve.php
+++ web/Week05/TeamActivity05/retrieve.php
@@ -23,11 +23,28 @@
           echo 'Error!: ' . $ex->getMessage();
           die();
         }
+
+   /*     try
+        {
+            $user = 'postgres';
+            $password = '';
+            $db = new PDO('pgsql:host=localhost;dbname=TeamActivity05', $user, $password);
+
+            // this line makes PDO give us an exception when there are problems,
+            // and can be very helpful in debugging! (But you would likely want
+            // to disable it for production environments.)
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $ex)
+        {
+            echo 'Error!: ' . $ex->getMessage();
+            die();
+        }*/
         
         if (isset($db))
         {
             $statement = $db->query('SELECT * FROM scriptures');
-            $resultSet = $statement->fetchAll(PDO::FETCH_ASSOC)
+            $resultSet = $statement->fetchAll(PDO::FETCH_ASSOC);
         }
     ?>
 
