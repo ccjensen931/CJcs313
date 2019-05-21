8376d855c95fa94d3507fdcd26aa2f9cf1de166f
Christian Jensen <ccjensen1179@gmail.com>
2019-05-21 16:38:24 -0600

    Adding search bar with variable query.

--- web/Week05/TeamActivity05/retrieve.php
+++ web/Week05/TeamActivity05/retrieve.php
@@ -40,8 +40,21 @@
             echo 'Error!: ' . $ex->getMessage();
             die();
         }*/
-        
-        if (isset($db))
+
+        $search = false;
+        if(isset($_POST["Search"]))
+        {
+            $book = $_POST["Search"];
+            $search = true;
+        }
+
+        if (isset($db) && $search)
+        {
+            $statement = $db->prepare('SELECT * FROM scriptures WHERE book=:book');
+            $statement->execute(array(':book' => $book));
+            $resultSet = $statement->fetchAll(PDO::FETCH_ASSOC);
+        }
+        else if (isset($db))
         {
             $statement = $db->query('SELECT * FROM scriptures');
             $resultSet = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -54,12 +67,17 @@
     <title>Team Activity 05</title>
 </head>
 <body>
+    <form action="retrieve.php" method="post" class="form-inline ml-3 my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="text" name="Search" placeholder="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <h1> Scripture Resources </h1>
     <?php
         if (isset($resultSet))
         {
             foreach ($resultSet as $row)
             {
-                echo '<p><b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b> - \"' . $row['content'] . '\"</p>\n';
+                echo '<p><b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b> - "' . $row['content'] . '"</p>';
             }
         }
     ?>
