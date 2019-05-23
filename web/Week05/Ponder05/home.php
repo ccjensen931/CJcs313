e13f03eeb0099d51fbc058b5d1508838df0345a6
Christian Jensen <ccjensen1179@gmail.com>
2019-05-23 16:10:20 -0600

    Starting Ponder05

---
+++ web/Week05/Ponder05/home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        session_start();
+        include 'dbConnect.php';
+
+        $loginURL = 'login.php';
+        $username;
+
+        if (isset($_SESSION["Username"]))
+        {
+            $username = $_SESSION["Username"];
+        }
+        else
+        {
+            header('Location: ' . $loginURL);
+            die();
+        }
+    ?>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <p>This is the home page!</p>
+</body>
+</html>
---
+++ web/Week05/Ponder05/login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        session_start();
+        include 'dbConnect.php';
+
+        if (isset($db) && isset($_POST["username"]) && isset($_POST["password"]))
+        {
+            $homeURL = 'home.php';
+
+            $statement = $db->prepare('SELECT user_password FROM users WHERE username=:username');
+            $statement->execute(array(':username' => $_POST["username"]));
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+            if (isset($result) && $result['user_password'] == $_POST["password"])
+            {
+                $_SESSION["Username"] = $_POST["username"];
+                header('Location: ' . $homeURL);
+                die();
+            }
+        }
+        else
+        {
+            
+        }
+    ?>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Login</h4>
+            <form action="login.php" method="post">
+                <div class="form-group">
+                    <label for=""></label>
+                    <div class="form-group">
+                        <label for="">Username</label>
+                        <input type="text"
+                            class="form-control" name="username" id="username" aria-describedby="helpId" placeholder="">
+                        <small id="helpId" class="form-text text-muted">Help text</small>
+                    </div>
+                    <div class="form-group">
+                    <label for="">Password</label>
+                        <input type="password" class="form-control" name="password" id="password" placeholder="">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
