db35dcdd1ae818d8532ba77f1d3365bcad9692c6
Christian Jensen <ccjensen1179@gmail.com>
2019-05-24 09:39:06 -0600

    Dropdown still doesn't work

--- web/Week05/Ponder05/navbar.php
+++ web/Week05/Ponder05/navbar.php
@@ -9,7 +9,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
+            <ul class="navbar-nav mr-3">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Menu <span class="navbar-toggler-icon"></span>
