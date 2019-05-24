d31a3a44c72dafc2b3605faa202f6632c1832b7f
Christian Jensen <ccjensen1179@gmail.com>
2019-05-24 09:27:05 -0600

    Trying to fix dropdown button.

--- web/Week05/Ponder05/navbar.php
+++ web/Week05/Ponder05/navbar.php
@@ -5,17 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Navbar</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item dropdown">
+                    <button class="navbar-toggler">
+                        <span class="navbar-toggle-icon"></span>
+                    </button>
+
                     <button class="navbar-toggler" type="button" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-controls="navbarDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
