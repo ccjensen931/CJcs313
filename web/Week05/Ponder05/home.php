afe857671b1599140f54585d90a6fd59de8c6fcb
ccjensen931 <ccjensen1179@gmail.com>
2019-05-24 21:23:07 -0600

    Fixing slight html error.

--- web/Week05/Ponder05/home.php
+++ web/Week05/Ponder05/home.php
@@ -47,9 +47,9 @@
                             echo '<li class="list-group-item">';
 
                             if (!$row['read'])
-                                echo '<p style="color:red;float:left;"> NEW </p><p style="float:right;>';
+                                echo '<p style="color:red;float:left;"> NEW </p>';
 
-                            echo $row['username'] . '</p></li>';
+                            echo '<p style="float:right;>' . $row['username'] . '</p></li>';
                         }
                     }
                 ?>
