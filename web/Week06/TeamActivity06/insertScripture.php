21d398d94420e96e7e4faf930a38218c9815ff9d
Christian Jensen <ccjensen1179@gmail.com>
2019-05-28 16:31:55 -0600

    Finally fixed margins

--- web/Week06/TeamActivity06/insertScripture.php
+++ web/Week06/TeamActivity06/insertScripture.php
@@ -34,8 +34,8 @@
                 {
                     foreach ($resultSet as $row)
                     {
-                        echo '<label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input" name="Topics[]" id="' . $row['topic']. '" value="' . $row['topic'] . '"> ' .
+                        echo '<label class="form-check-label ml-5">
+                                  <input type="checkbox" class="form-check-input ml-5" name="Topics[]" id="' . $row['topic']. '" value="' . $row['topic'] . '"> ' .
                                   $row['topic'] .
                               '</label><br>';
                     }
