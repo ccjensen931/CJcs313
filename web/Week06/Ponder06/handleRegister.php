171afc6695eb83ca25adec47b586a2505796acef
ccjensen931 <ccjensen1179@gmail.com>
2019-05-30 10:08:11 -0600

    Removing unnecessary HTML from handleRegister.php

--- web/Week06/Ponder06/handleRegister.php
+++ web/Week06/Ponder06/handleRegister.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <?php
     include 'dbConnect.php';
 
@@ -16,15 +7,7 @@
     {
         $statement = $db->prepare("INSERT INTO users VALUES (nextval('users_s1'), :username, :pass, :email, :first_name, :last_name);");
         $statement->execute(array(':username' => $_POST['Username'], ':pass' => $_POST['Password'], ':email' => $_POST['Email'], ':first_name' => $_POST['First_Name'], ':last_name' => $_POST['Last_Name']));
-        $newID = $db->lastInsertId('users_s1');
-
-        echo '<p>' . $newID . '</p>';
-    }
-    else{
-        echo '<p>Something was missing!</p>';
     }
 
-    //header('Location: ' . $loginURL);
+    header('Location: ' . $loginURL);
 ?>
-</body>
-</html>
