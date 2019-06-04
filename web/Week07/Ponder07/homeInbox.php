79c449052d9744bef233c19a72e051bd1329a612
ccjensen931 <ccjensen1179@gmail.com>
2019-06-04 12:03:33 -0600

    Updating message read stuff

--- web/Week07/Ponder07/homeInbox.php
+++ web/Week07/Ponder07/homeInbox.php
@@ -15,7 +15,8 @@
         }
         if (isset($_GET["id"]))
         {
-            echo 'Message id ' . $_GET["id"];
+            $updateMessagesStatement = $db->prepare('UPDATE messages SET message_read=:read WHERE message_id=:id');
+            $updateMessagesStatement->execute(array(':read' => true, ':id' => $_GET["id"]));
         }
     ?>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
