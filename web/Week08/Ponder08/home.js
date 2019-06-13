b9112245a48e4f8d86f2eeb8aad760a24293a498
Christian Jensen <ccjensen1179@gmail.com>
2019-06-13 16:44:01 -0600

    Ponder 08

--- web/Week08/Ponder08/home.js
+++ web/Week08/Ponder08/home.js
@@ -1,11 +1,21 @@
 let http = require ('http');
+let url = require('url');
 
-http.createServer(function (req, res) {
-    res.write('Hello World!');
-    res.end();
-}).listen(8888);
+http.createServer(onRequest).listen(8888);
 
 function onRequest(req, res)
 {
+    let body = "";
 
+    if (url.parse(req.url, true).pathname == "/home") {
+        body = '<h1>Welcome to the Home Page</h1>';
+        res.writeHead(200, {'Content-type' : 'text/html'}).end(body);
+    } else if (url.parse(req.url, true).pathname == "/getData") {
+        res.writeHead(200, {'Content-type' : 'text/html'});
+        res.write(JSON.stringify({"name" : "Christian Jensen", "class" : "CS 313"}));
+        res.end();
+    } else {
+        body = '<h1>Page Not Found</h1>';
+        res.writeHead(404, {'Content-type' : 'text/html'}).end(body);
+    }
 }
